ae3cc2a4fb172e7f3a8ace5629385b17874115fe
Nidhi Singh <nidhi232singh@gmail.com>
2021-08-31 16:02:12 +0530

    JavaLiteral + DynamicInitialization

---
+++ src/Exp3/DynamicInitialization.java
@@ -0,0 +1,23 @@
+package Exp3;
+
+public class DynamicInitialization {
+
+    public static void main(String args[]) {
+        double a = 3.0, b = 4.0f;
+
+        // c is dynamically initialized
+        double c = Math.sqrt(a * a + b * b);
+        double d = c * a;
+        System.out.println("Hypotenuse is " + c);
+
+        System.out.println("value of d " + d);
+
+    }
+}
+
+
+
+
+
+
+
---
+++ src/Exp3/Scope.java
@@ -0,0 +1,10 @@
+package Exp3;
+
+public class Scope {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
