c121b437ffeefd5e92a8cfbfbbedde995b867eda
Nidhi Singh <nidhi232singh@gmail.com>
2021-08-31 12:06:09 +0530

    Stack+Queue

---
+++ src/Queue/QueueClient.java
@@ -0,0 +1,29 @@
+package Queue;
+
+public class QueueClient {
+
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+
+
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        queue.display();
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        queue.display();
+
+        queue.enqueue(60);
+        queue.enqueue(70);
+
+        queue.display();
+    }
+
+}
--- src/Stack/StackClient.java
+++ src/Stack/StackClient.java
@@ -1,4 +1,24 @@
 package Stack;
 
 public class StackClient {
+
+    public static void main(String[] args) throws Exception{
+        // TODO Auto-generated method stub
+
+        Stack s = new Stack(4);
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+
+        s.display();
+
+        s.pop();
+        s.pop();
+
+        s.display();
+
+    }
+
 }
